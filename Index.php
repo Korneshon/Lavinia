72a038f4d62433a1cf1882ab0cbd2eeab79a5123
Korneshon <148193963+Korneshon@users.noreply.github.com>
2025-05-29 13:02:02 +0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,28 @@
+
+<?php
+include 'header.php' ?>
+    <div class="ogl">
+        <h3>Оглавление</h3>
+<p><a href='laviniya_zharosvet.php'>Лавиния Жаросвет</a></p>
+<p><a href='istoriya.php'>История</a></p>
+<p><a href='semya.php'>Семья</a></p>
+<p><a href='tovarishchi.php'>Товарищи</a></p>
+<p><a href='anzhaks.php'>Ан'жакс</a></p>
+<p><a href='tav.php'>Тав</a></p>
+<p><a href='tocha.php'>Точа</a></p>
+<p><a href='takeo.php'>Такео</a></p>
+<p><a href='belion.php'>Бэлион</a></p>
+<p><a href='pyoryshko.php'>Пёрышко</a></p>
+<p><a href='kliriya.php'>Клирия</a></p>
+<p><a href='selvara.php'>Сельвара</a></p>
+<p><a href='strad_fon_zarovich.php'>Страд фон Зарович</a></p>
+<p><a href='marianna.php'>Марианна</a></p>
+<p><a href='blinskiy.php'>Блинский</a></p>
+<p><a href='bella.php'>Бэлла</a></p>
+<p><a href='baroviya.php'>Баровия</a></p>
+<p><a href='vallaki.php'>Валлаки</a></p>
+<p><a href='derevnya.php'>Деревня</a></p>
+<p><a href='berloga.php'>Берлога</a></p>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <header>
+        <h2><a href=""> ОГЛАВЛЕНИЕ </a></h2>
+                <h1>/</h1>
+        <h2><a href="Places.php"> МЕСТА </a></h2>
+        <h2><a href="Characters.php">ПЕРСОНАЖИ</a></h2>
+
+        <h2><a href="Ivents.php">СОБЫТИЯ</a></h2>
+    </header>
