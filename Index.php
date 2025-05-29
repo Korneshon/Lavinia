c2bf8efede4d0654f0e9052fbf2309d7a45dbeb1
Korneshon <148193963+Korneshon@users.noreply.github.com>
2025-05-29 13:03:53 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,7 +1,25 @@
 
-<?php
-include 'header.php' ?>
-    <div class="ogl">
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <header>
+        <h2><a href=""> ОГЛАВЛЕНИЕ </a></h2>
+                <h1>/</h1>
+        <h2><a href="Places.php"> МЕСТА </a></h2>
+        <h2><a href="Characters.php">ПЕРСОНАЖИ</a></h2>
+
+        <h2><a href="Ivents.php">СОБЫТИЯ</a></h2>
+    </header>
         <h3>Оглавление</h3>
 <p><a href='laviniya_zharosvet.php'>Лавиния Жаросвет</a></p>
 <p><a href='istoriya.php'>История</a></p>
